6383bd017c64b2bba3dbd319deae0a0383e2aff6
Park Ju Hyung <qkrwngud825@gmail.com>
2015-08-03 23:11:58 +0900

    arm64: use GCC 4.9
    GCC 4.8 is not compatible with Android's arm64 nature.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- core/combo/TARGET_linux-arm64.mk
+++ core/combo/TARGET_linux-arm64.mk
@@ -35,10 +35,10 @@ TARGET_ARCH_VARIANT := armv8
 endif
 
 # Decouple NDK library selection with platform compiler version
-TARGET_NDK_GCC_VERSION := 4.8
+TARGET_NDK_GCC_VERSION := 4.9
 
 ifeq ($(strip $(TARGET_GCC_VERSION_EXP)),)
-TARGET_GCC_VERSION := 4.8
+TARGET_GCC_VERSION := 4.9
 else
 TARGET_GCC_VERSION := $(TARGET_GCC_VERSION_EXP)
 endif
